9d060a4b81c65b887a042845a0e4c0e2c43a2653
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-13 09:57:14 -0800

    Header file created, first 0-printf.c draft

---
+++ 0-printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string with conversion specifiers
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Custom printf project
+Project to be done in teams of 2 people (your team: Olayya Mansour, Hamza Yassine)
