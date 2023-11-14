ba53be928ac34f89e74f04d65e11c3cea01b76f3
olayyaMansour <mansour.olayya@gmail.com>
2023-11-14 09:55:02 -0800

    update main.h

--- main.h
+++ main.h
@@ -1,37 +1,15 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
 
-/**
- * _printf - Custom printf function
- * @format: Format string with conversion specifiers
- *
- * Return: Number of characters printed (excluding null byte)
- */
-int _printf(const char *format, ...)
-{
-	int printedCharCount = 0;
-	va_list args;
+int _printf(const char *format, ...);
+int printChar(char c);
+int printString(char *str);
+int handleSpecifier(char string, va_list args);
 
-	va_start(args, format);
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			printedCharCount += handleSpecifier(*format, args);
-		}
-		else
-		{
-			printedCharCount += printChar(*format);
-		}
-		format++;
-	}
-	va_end(args);
-
-	return (printedCharCount);
-}
-
+#endif
 
