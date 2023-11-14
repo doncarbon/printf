78ffc5f16efd17aaf018f2fe3bc425669840c1b4
olayyaMansour <mansour.olayya@gmail.com>
2023-11-14 11:35:16 -0800

    Update function comment and betty style in printBinary

--- print_binary.c
+++ print_binary.c
@@ -1,31 +1,41 @@
 #include "main.h"
+
+/**
+ * printBinary	- prints a binary number from an unsigned int argument
+ * @args: parameter.
+ * Return: integer
+ */
+
 int printBinary(va_list args)
 {
-    int isFirstNonZeroBitFound = 0;
-    int characterCount = 0;
-    int i, bitmask = 1, binaryDigit;
-    unsigned int number = va_arg(args, unsigned int);
-    unsigned int bitCheck;
+	int isFirstNonZeroBitFound = 0;
+	unsigned int number = va_arg(args, unsigned int);
+	unsigned int bitCheck;
+	int bitmask = 1;
+	int binaryDigit;
+	int characterCount = 0;
 
-    for (i = 31; i >= 0; i--)
-    {
-        bitCheck = ((bitmask << i) & number);
-        if (bitCheck >> i)
-            isFirstNonZeroBitFound = 1;
-        if (isFirstNonZeroBitFound)
-        {
-            binaryDigit = bitCheck >> i;
-            printChar(binaryDigit + '0');
-            characterCount++;
-        }
-    }
+	int i;
 
-    if (characterCount == 0)
-    {
-        characterCount++;
-        printChar('0');
-    }
+	for (i = 31; i >= 0; i--)
+	{
+		bitCheck = ((bitmask << i) & number);
+		if (bitCheck >> i)
+			isFirstNonZeroBitFound = 1;
+		if (isFirstNonZeroBitFound)
+		{
+			binaryDigit = bitCheck >> i;
+			printChar(binaryDigit + '0');
+			characterCount++;
+		}
+	}
 
-    return characterCount;
+	if (characterCount == 0)
+	{
+		characterCount++;
+		printChar('0');
+	}
+
+	return (characterCount);
 }
 
